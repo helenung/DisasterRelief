668a8726b83dcdeb3f8730d3fae4fc2cd757f79f
Jeannette Yu <nette.vi@gmail.com>
2014-10-11 15:28:26 -0700

    begin button

--- relief.js
+++ relief.js
@@ -4,7 +4,8 @@ function preload() {
 	game.load.image('bg', 'bg.png');
 	game.load.image('toRope', 'rope_button.png');
 	game.load.image('ropeBg', 'rope_bg.png');
-	game.load.image('back', 'back_button.png')
+	game.load.image('back', 'back_button.png');
+	game.load.image('begin', 'begin_button.png')
 }
 
 function create() {
@@ -14,13 +15,22 @@ function create() {
 	toRope.events.onInputUp.add(function() {
 		ropeScreen = game.add.sprite(0, 0, 'ropeBg');
 		backButton = game.add.sprite(800, 525, 'back');
+		beginButton = game.add.sprite(350, 225, 'begin');
 		backButton.inputEnabled = true;
-		backButton.events.onInputUp.add(toMenu);
+		backButton.events.onInputUp.add(endRope);
+		beginButton.inputEnabled = true;
+		beginButton.events.onInputUp.add(startRope);
 	});
 }
 
-function toMenu() {
+function endRope() {
 	ropeScreen.destroy();
+	beginButton.destroy();
+	backButton.destroy();
+}
+
+function startRope() {
+	beginButton.destroy();
 	backButton.destroy();
 }
 
