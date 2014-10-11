febb16fa472db06e55b556c596895fe38350bc85
Jeannette Yu <nette.vi@gmail.com>
2014-10-11 14:27:01 -0700

    button kinda works

--- relief.js
+++ relief.js
@@ -2,10 +2,17 @@ var game = new Phaser.Game(900, 600, Phaser.CANVAS, 'game', {preload: preload, c
 
 function preload() {
 	game.load.image('bg', 'bg.png');
+	game.load.image('toRope', 'rope_button.png');
+	game.load.image('ropeBg', 'rope_bg.png');
 }
 
 function create() {
 	game.add.sprite(0, 0, 'bg');
+	var toRope = game.add.sprite(210, 250, 'toRope');
+	toRope.inputEnabled = true;
+	toRope.events.onInputUp.add(function() {
+		game.add.sprite(0, 0, 'ropeBg');
+	});
 }
 
 function update() {
