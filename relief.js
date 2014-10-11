9a4f1512099a983b880b72be938ff9519ba06781
Jeannette Yu <nette.vi@gmail.com>
2014-10-11 16:28:29 -0700

    boulder page

--- relief.js
+++ relief.js
@@ -6,6 +6,8 @@ function preload() {
 	game.load.image('ropeBg', 'rope_bg.png');
 	game.load.image('back', 'back_button.png');
 	game.load.image('begin', 'begin_button.png')
+	game.load.image('boulderBg', 'boulder_bg.png');
+	game.load.image('toBoulder', 'boulder_button.png');
 	game.load.image('rock', 'rock.png');
 	game.load.image('rockSmash', 'rockSmash.png');
 }
@@ -15,18 +17,30 @@ function create() {
 	toRope = game.add.sprite(210, 250, 'toRope');
 	toRope.inputEnabled = true;
 	toRope.events.onInputUp.add(function() {
-		ropeScreen = game.add.sprite(0, 0, 'ropeBg');
-		backButton = game.add.sprite(800, 525, 'back');
-		beginButton = game.add.sprite(350, 225, 'begin');
-		backButton.inputEnabled = true;
-		backButton.events.onInputUp.add(endRope);
-		beginButton.inputEnabled = true;
-		beginButton.events.onInputUp.add(startRope);
+		miniGame('ropeBg', startRope);
+	});
+	toBoulder = game.add.sprite(400, 140, 'toBoulder');
+	toBoulder.inputEnabled = true;
+	toBoulder.events.onInputUp.add(function() {
+		miniGame('boulderBg', startRescue)
 	});
 }
 
-function endRope() {
-	ropeScreen.destroy();
+function miniGame(background, minigame) {
+	thisBg = game.add.sprite(0, 0, background);
+	backButton = game.add.sprite(800, 525, 'back');
+	beginButton = game.add.sprite(350, 225, 'begin');
+	backButton.inputEnabled = true;
+	backButton.events.onInputUp.add(function() {
+		endGame(thisBg);
+	});
+	beginButton.inputEnabled = true;
+	beginButton.events.onInputUp.add(minigame, this);
+
+}
+
+function endGame(background) {
+	background.destroy();
 	beginButton.destroy();
 	backButton.destroy();
 }
