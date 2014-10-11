aea5b0ba5e3a94defab4761ef5431a254fc6d89b
helen <uhelen94@gmail.com>
2014-10-11 14:30:14 -0700

    stuff
    stuff

--- relief.js
+++ relief.js
@@ -1,13 +1,31 @@
-var game = new Phaser.Game(900, 600, Phaser.CANVAS, 'game', {preload: preload, create: create, update: update });
+(function() {
 
-function preload() {
-	game.load.image('bg', 'bg.png');
-}
+	window.onload = function() {
+		document.getElementById("game").onclick = begin;
 
-function create() {
-	game.add.sprite(0, 0, 'bg');
-}
+	};
 
-function update() {
+	var game = new Phaser.Game(900, 600, Phaser.CANVAS, 'game', {preload: preload, create: create, update: update });
 
-}
+	function begin() {
+		 var text = "Welcome to the game!";
+    	var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+
+    	var t = game.add.text(game.world.centerX-300, 0, text, style);
+	}
+
+	
+
+	function preload() {
+		game.load.image('bg', 'bg.png');
+	}
+
+	function create() {
+		game.add.sprite(0, 0, 'bg');
+	}
+
+	function update() {
+
+	}
+
+})();
